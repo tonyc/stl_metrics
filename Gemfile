325ef56390f978e409e9928a2cb969a9d6de0f69
Tony Collen <tony@halogenlabs.com>
2020-03-29 12:40:34 -0500

    Turn gemspec back on, add display_metrics.rb script

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source "https://rubygems.org"
 
+gemspec
+
 gem "rake"
 gem "rspec", "~> 3.0"
 gem "awesome_print"
---
+++ display_metrics.rb
@@ -0,0 +1,11 @@
+require "stl_metrics"
+
+raise ArgumentError, "Please supply a file name" unless filename = ARGV[0]
+raise ArgumentError, "File doesn't exist: #{filename}" unless File.exist?(filename)
+
+puts "Parsing: #{filename}"
+result = StlMetrics.parse_file(filename)
+
+puts " Total facets: #{result.total_facets}"
+puts " Surface area: #{result.surface_area}"
+puts " Bounding box: (not implemented)"
